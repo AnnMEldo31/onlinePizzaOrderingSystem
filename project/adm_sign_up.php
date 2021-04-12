e72fce6e04ecbaa6c22370e0e4d19cae616eed22
Nanotech1611 <62535428+Nanotech1611@users.noreply.github.com>
2021-04-12 11:53:32 +0530

    heh

--- project/adm_sign_up.php
+++ project/adm_sign_up.php
@@ -1,13 +1,13 @@
 <?php
-    $Username=$_POST['Username'];
-    $password=$_POST['password'];
+    $a_user=$_POST['username'];
+    $a_pass=$_POST['password'];
     //database connection
     $con = new mysqli('localhost','root','','dbms_project');
     if($con->connect_error){
         die("failed to connect :" .$con->connect_error);
     }else{
         $stmt = $con->prepare("insert into Admin_acct(a_user,a_pass)values(?,?)");
-        $stmt->bind_para("ss",$username,$password);
+        $stmt->bind_para("ss",$a_user,$a_pass);
         $stmt->execute();
         echo "login successfull...";
         $stmt->close();
