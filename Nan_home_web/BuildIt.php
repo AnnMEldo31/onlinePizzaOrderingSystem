7506f494305e4d9af643ccb1430f3fa2ab6c138d
Nanotech1611 <62535428+Nanotech1611@users.noreply.github.com>
2021-04-12 18:19:01 +0530

    build it worksss broo
    wooohooo

--- Nan_home_web/BuildIt.php
+++ Nan_home_web/BuildIt.php
@@ -20,65 +20,66 @@
         $topping = implode('\',\'', $toppingarray);
     
         //cust_ID get from importings from login thingy..... include 'login.php';
-        $sql_query1 = "INSERT INTO Orders (Cust_ID) VALUES('1')"; //initiating order_ID
-        if(mysqli_query($conn, $sql_query1)){
-            echo "successful checkout1";
-        }
-        else{
-            echo "Error: " . $sql_query1 . "" . mysqli_error($conn);
-        }
+        $sql_query1 = mysqli_query($conn,"INSERT INTO Orders (Cust_ID) VALUES('1')"); //initiating order_ID
 
-        $orderIDobj = mysqli_query($conn,"SELECT max(Order_ID) FROM Orders where Cust_ID=1 and Total_Price=0.00;");//getting order_ID
-        $row=mysqli_fetch_assoc($orderIDobj);
-        echo $row["max(OrderID)"];
+        $orderIDobj = mysqli_query($conn,"SELECT Order_ID FROM Orders where order_id IN(SELECT max(order_ID) from orders where Cust_ID=1);");//getting order_ID
+        while($row = mysqli_fetch_array($orderIDobj)){
+            $rowOrderID = $row["Order_ID"];                     //new Order_ID
+        }
 
         //calculating b_Price.....price of each pizza
-            //get sum of all selected ingredient ka price from ingredients table
-            $pizza_B_price= mysqli_query($conn,"SELECT SUM(In_Price) from ingredients where In_Name IN('$topping')");
-            
+            //get sum of all selected ingredients ka price from ingredients table
+            $pizza_B_price= mysqli_query($conn,"SELECT SUM(In_Price) as sum1 from ingredients where In_Name IN('$topping','$crust','$sauce','$cheese');");
+            while($row = mysqli_fetch_array($pizza_B_price)){
+                    $rowPrice = $row["sum1"];                   //new B_Price
+                }
 
-        $sql_query2 = "INSERT INTO Bill_Items (Cust_ID, Order_ID, B_Price) VALUES('1', $orderIDvar, (string)$pizza_B_price)";
-        if(mysqli_query($conn, $sql_query2)){
-            echo "successful checkout4";
-        }
-        else{
-            echo "Error: " . $sql_query2 . "" . mysqli_error($conn);
-        }
+        $sql_query2 = mysqli_query($conn,"INSERT INTO Bill_Items (Cust_ID, Order_ID, B_Price) VALUES('1', '$rowOrderID', '$rowPrice');");
 
         //getting Pizza_ID from Bill_Items
-        $PizzaIDvar = "SELECT Pizza_ID FROM Bill_Items where Order_ID = $orderIDvar";
-        mysqli_query($conn, $PizzaIDvar);
+        $PizzaIDobj = mysqli_query($conn, "SELECT Pizza_ID FROM Bill_Items where Order_ID = $rowOrderID;");
+        while($row = mysqli_fetch_array($PizzaIDobj)){
+            $PizzaIDvar = $row["Pizza_ID"];                     //new Pizza_ID
+        }
+        
+        //getting crust_ID, Sauce_ID, Cheese_ID
+        $crust_query = (mysqli_query($conn,"SELECT Ingr_ID from ingredients where In_Name = '$crust'"));
+        while($row = mysqli_fetch_array($crust_query)){
+            $Crust_ID = $row["Ingr_ID"];                     //new Crust_ID
+        }
+
+        $cheese_query = (mysqli_query($conn,"SELECT Ingr_ID from ingredients where In_Name = '$cheese'"));
+        while($row = mysqli_fetch_array($cheese_query)){
+            $Cheese_ID = $row["Ingr_ID"];                     //new Cheese_ID
+        }
+
+        $sauce_query = (mysqli_query($conn,"SELECT Ingr_ID from ingredients where In_Name = '$sauce'"));
+        while($row = mysqli_fetch_array($sauce_query)){
+            $Sauce_ID = $row["Ingr_ID"];                     //new Sauce_ID
+        }
 
         //inserting crust, sauce, cheese
-        $sql_query3 = "INSERT INTO needs (Pizza_ID, Ingr_ID) VALUES('$PizzaIDvar', '$crust')";
-        mysqli_query($conn, $sql_query3);
-        $sql_query4 = "INSERT INTO needs (Pizza_ID, Ingr_ID) VALUES('$PizzaIDvar', '$sauce')";
+        $sql_query3 = mysqli_query($conn,"INSERT INTO needs (Pizza_ID, Ingr_ID) VALUES('$PizzaIDvar', '$Crust_ID')");
+
+        $sql_query4 = "INSERT INTO needs (Pizza_ID, Ingr_ID) VALUES('$PizzaIDvar', '$Sauce_ID')";
         mysqli_query($conn, $sql_query4);
-        $sql_query5 = "INSERT INTO needs (Pizza_ID, Ingr_ID) VALUES('$PizzaIDvar', '$cheese')";
+        $sql_query5 = "INSERT INTO needs (Pizza_ID, Ingr_ID) VALUES('$PizzaIDvar', '$Cheese_ID')";
         mysqli_query($conn, $sql_query5);
 
         //inserting all selected toppings
-        if(!empty($topping)) {
+        if(!empty($toppingarray)) {
             $i=0;
-            while($topping[$i]) {
-                $sql_queryn = "INSERT into needs(Pizza_ID,Ingr_ID) values";
-                $sql_queryn .= "(".$PizzaIDvar.",";
-                $toppingIDvar = "SELECT Ingr_ID from ingredients where Ingr_ID=\'".$topping[$i]."\.";
-                $sql_queryn.= $toppingIDvar.")";
-                mysqli_query($conn, $sql_queryn);
-                $i += 1;
+            $arrayLength = count($toppingarray);
+            while($i<$arrayLength){
+                $topping = $toppingarray[$i];
+                $toppingIDobj = mysqli_query($conn,"SELECT Ingr_ID from ingredients where In_Name='$topping';");
+                while($row = mysqli_fetch_array($toppingIDobj)){
+                    $topping_ID = $row["Ingr_ID"];                     //new topping_ID
+                }
+                $i+=1;
+                $sql_queryn = mysqli_query($conn,"INSERT into needs(Pizza_ID,Ingr_ID) values('$PizzaIDvar','$topping_ID')");
+                
             }
         }
-
-
-
-        if(mysqli_query($conn, $sql_queryn)){
-            echo "successful checkout";
-        }
-        else{
-            echo "Error: " . $sql_queryn . "" . mysqli_error($conn);
-        }
-
-        mysqli_close($conn);
-    }  
+    }
 ?>
